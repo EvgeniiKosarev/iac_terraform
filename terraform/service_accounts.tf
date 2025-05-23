ddfa2b850ced8c1e8c53564002593be26a874a94
evgenii.kosarev <evgenii.kosarev@crystalloids.com>
2025-05-24 00:02:09 +0200

    add service account module

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -1,3 +1,5 @@
+######### sample-db-app-actions-sa@clgcporg10-190.iam.gserviceaccount.com #########
+
 resource "google_service_account" "sample-db-app-actions-sa" {
   account_id   = "sample-db-app-actions-sa"
   display_name = "SA for Github Workflow for Application"
@@ -8,13 +10,8 @@ resource "google_project_iam_member" "sample-db-app-actions-sa_kubernetes" {
   role    = "roles/container.admin"
   member  = "serviceAccount:${google_service_account.sample-db-app-actions-sa.email}"
 }
-resource "google_project_iam_member" "sample-db-app-actions-sa_storage" {
-  project = var.project_id
-  role    = "roles/storage.objectCreator"
-  member  = "serviceAccount:${google_service_account.sample-db-app-actions-sa.email}"
-}
 
-
+######### mongodb-vm-sa@clgcporg10-190.iam.gserviceaccount.com #########
 
 resource "google_service_account" "mongodb-vm-sa" {
   account_id   = "mongodb-vm-sa"

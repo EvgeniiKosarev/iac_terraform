513f8f8cea9835594640158fdebe7de0e36b8199
evgenii.kosarev <evgenii.kosarev@crystalloids.com>
2025-05-23 23:45:28 +0200

    add service account module

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -9,5 +9,19 @@ resource "google_project_iam_member" "sample-db-app-actions-sa_kubernetes" {
   member  = "serviceAccount:${google_service_account.sample-db-app-actions-sa.email}"
 }
 
+resource "google_service_account" "mongodb-vm-sa" {
+  account_id   = "mongodb-vm-sa"
+  display_name = "SA for VM for MongoDB"
+}
 
+resource "google_project_iam_member" "mongodb-vm-sa_storage" {
+  project = var.project_id
+  role    = "roles/storage.objectCreator"
+  member  = "serviceAccount:${google_service_account.mongodb-vm-sa.email}"
+}
 
+resource "google_project_iam_member" "mongodb-vm-sa_editor" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.mongodb-vm-sa.email}"
+}

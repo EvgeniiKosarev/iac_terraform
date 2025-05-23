d4285dec2197b346f5d912da331c57fa6fd06b7a
evgenii.kosarev <evgenii.kosarev@crystalloids.com>
2025-05-23 20:04:26 +0200

    use github secrets for mongodb access

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,7 @@ resource "google_compute_instance" "db_vm" {
 resource "google_container_cluster" "gke_cluster" {
   name     = "cluster-for-evgenii-app"
   location = var.region
-  node_locations = var.zone
+  node_locations = ["europe-west1-b"]
   remove_default_node_pool = true
   initial_node_count       = 1
 
